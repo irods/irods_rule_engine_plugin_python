1b666c397fa176bc0bcc957841c053de08b4fb2d
Rick Skarbez <skarbez@gmail.com>
2016-11-30 18:14:59 -0500

    [#2] Added rulemsiDeleteUnusedAVUs.r

---
+++ python_rules/rulemsiDeleteUnusedAVUs.r
@@ -0,0 +1,6 @@
+def myTestRule(rule_args, callback):
+    condition = global_vars['*arg1'][1:-1]
+    callback.delayExec(condition, 'callback.msiDeleteUnusedAVUs()', '')
+
+INPUT *arg1 = "<PLUSET>1m</PLUSET><EF>24h</EF>"
+OUTPUT ruleExecOut
