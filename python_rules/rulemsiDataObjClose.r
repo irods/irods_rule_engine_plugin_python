41c40e6ebfd5a5c7aeadf8feb5c4a7db7618c515
Rick Skarbez <skarbez@gmail.com>
2016-11-30 16:16:01 -0500

    [#2] Added rulemsiDataObjClose.r

---
+++ python_rules/rulemsiDataObjClose.r
@@ -0,0 +1,15 @@
+def myTestRule(rule_args, callback):
+    resc = global_vars['*Resc'][1:-1]
+    obj = global_vars['*Obj'][1:-1]
+    oflags = global_vars['*OFlags'][1:-1]
+
+    dummy_int = {}
+    dummy_int[PYTHON_MSPARAM_TYPE] = PYTHON_INT_MS_T
+
+    ret_val = callback.msiDataObjCreate(obj, oflags, dummy_int)
+    file_desc = ret_val[PYTHON_RE_RET_OUTPUT][2]
+    callback.writeLine('stdout', 'Created and closed file ' + obj)
+    callback.msiDataObjClose(file_desc, dummy_int)
+    
+INPUT *Resc="demoResc", *Obj="/tempZone/home/rods/test/foo4", *OFlags="destRescName=demoResc++++forceFlag="
+OUTPUT ruleExecOut
