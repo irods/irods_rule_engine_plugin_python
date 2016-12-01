07a330649e7e0d2912964f7d46e67afe0be37c37
Rick Skarbez <skarbez@gmail.com>
2016-12-01 16:29:46 -0500

    [#2] Added rulemsiSetACL.r

---
+++ python_rules/rulemsiSetACL.r
@@ -0,0 +1,11 @@
+def myTestRule(rule_args, callback):
+    user = global_vars['*User'][1:-1]
+    path = global_vars['*Path'][1:-1]
+    acl = global_vars['*Acl'][1:-1]
+
+    callback.msiSetACL('default', acl, user, path)
+
+    callback.writeLine('stdout', 'Set ' + acl + ' access for ' + user + ' on file ' + path)
+
+INPUT *User="rods", *Path="/tempZone/home/rods/sub1/foo3", *Acl="write"
+OUTPUT ruleExecOut
