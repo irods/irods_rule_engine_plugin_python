97f6483d39822dea3fbe02bbef395821ddfc0672
Rick Skarbez <skarbez@gmail.com>
2016-12-01 16:34:26 -0500

    [#2] Added rulemsiSetQuota.r

---
+++ python_rules/rulemsiSetQuota.r
@@ -0,0 +1,12 @@
+def myTestRule(rule_args, callback):
+    type = global_vars['*Type'][1:-1]
+    name = global_vars['*Name'][1:-1]
+    resource = global_vars['*Resource'][1:-1]
+    value = global_vars['*Value'][1:-1]
+
+    callback.msiSetQuota(type, name, resource, value)
+    
+    callback.writeLine('stdout', 'Set quota on ' + name + ' for resource ' + resource + ' to ' + value + ' bytes')
+
+INPUT *Type="user", *Name="rods", *Resource="demoResc", *Value="1000000000"
+OUTPUT ruleExecOut
