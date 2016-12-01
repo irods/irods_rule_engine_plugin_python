7deb240dd16e89cff345df5923d87a252887e2d9
Rick Skarbez <skarbez@gmail.com>
2016-12-01 16:08:34 -0500

    [#2] Added rulemsiQuota.r

---
+++ python_rules/rulemsiQuota.r
@@ -0,0 +1,6 @@
+def myTestRule(rule_args, callback):
+    callback.delayExec('<PLUSET>30s</PLUSET><EF>24h</EF>', 'callback.msiQuota()', '')
+    callback.writeLine('serverLog', 'Updated quota check')
+
+INPUT null
+OUTPUT ruleExecOut
