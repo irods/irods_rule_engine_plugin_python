f836fd271df5462278501d1929bd9febd8cd1968
Rick Skarbez <skarbez@gmail.com>
2016-12-01 15:13:02 -0500

    [#2] Added rulemsiGetTaggedValueFromString.r

---
+++ python_rules/rulemsiGetTaggedValueFromString.r
@@ -0,0 +1,14 @@
+def myTestRule(rule_args, callback):
+    tag = global_vars['*Tag'][1:-1]
+    in_str = global_vars['*Str'][1:-1]
+
+    callback.writeLine('stdout', 'String that is tested is')
+    callback.writeLine('stdout', in_str)
+
+    ret_val = callback.msiGetTaggedValueFromString(tag, in_str, 'dummy_str')
+    val = ret_val[PYTHON_RE_RET_OUTPUT][2]
+
+    callback.writeLine('stdout', 'Found value is ' + val)
+
+INPUT *Tag="Mail", *Str="<Mail>IP-address</Mail>"
+OUTPUT ruleExecOut
