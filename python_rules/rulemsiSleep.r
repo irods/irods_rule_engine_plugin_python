3b49548279a02b737a4b291d41d4f375a0b40047
Rick Skarbez <skarbez@gmail.com>
2016-12-01 18:24:24 -0500

    [#2] Added rulemsiSleep.r

---
+++ python_rules/rulemsiSleep.r
@@ -0,0 +1,13 @@
+def myTestRule(rule_args, callback):
+    sec = global_vars['*Sec'][1:-1]
+    microsec = global_vars['*MicroSec'][1:-1]
+
+    import time
+    callback.writeLine('stdout', time.ctime())
+    # callback.msiSleep(sec, microsec)
+    float_sec = float(sec) + float(microsec)/1000000
+    time.sleep(float_sec)
+    callback.writeLine('stdout', time.ctime())
+
+INPUT *Sec="3", *MicroSec="123456"
+OUTPUT ruleExecOut
