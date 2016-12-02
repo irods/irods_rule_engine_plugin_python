414a98301546a9d54f72527a0c126d24628c1b5f
Rick Skarbez <skarbez@gmail.com>
2016-12-02 13:27:00 -0500

    [#2] Added rulemsiTarFileCreate.r

---
+++ python_rules/rulemsiTarFileCreate.r
@@ -0,0 +1,12 @@
+def myTestRule(rule_args, callback):
+    file = global_vars['*File'][1:-1]
+    coll = global_vars['*Coll'][1:-1]
+    resc = global_vars['*Resc'][1:-1]
+    flag = global_vars['*Flag'][1:-1]
+
+    callback.msiTarFileCreate(file, coll, resc, flag)
+
+    callback.writeLine('stdout', 'Created tar file ' + file + ' for collection ' + coll + ' on resource ' + resc)
+
+INPUT *File="/tempZone/home/rods/test/testcoll.tar", *Coll="/tempZone/home/rods/sub1", *Resc="demoResc", *Flag=""
+OUTPUT ruleExecOut
