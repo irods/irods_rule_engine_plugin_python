1b1662dab2e9004c36a3076ff843e60dbe9ea430
Rick Skarbez <skarbez@gmail.com>
2016-12-02 14:58:06 -0500

    [#2] Added rulemsiAddKeyValToMspStr.r

---
+++ python_rules/rulemsiAddKeyValToMspStr.r
@@ -0,0 +1,11 @@
+def myTestRule(rule_args, callback):
+    attr_name = global_vars['*AttrName'][1:-1]
+    attr_value = global_vars['*AttrValue'][1:-1]
+
+    out_str = '='.join([attr_name, attr_value])
+
+    callback.writeLine('stdout', 'The string now contains')
+    callback.writeLine('stdout', out_str)
+
+INPUT *AttrName="destRescName", *AttrValue="demoResc"
+OUTPUT ruleExecOut
