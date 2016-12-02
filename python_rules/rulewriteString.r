a70f17f487223c05481c167bf731c2a6b8030b6f
Rick Skarbez <skarbez@gmail.com>
2016-12-02 13:49:52 -0500

    [#2] Added rulewriteString.r

---
+++ python_rules/rulewriteString.r
@@ -0,0 +1,8 @@
+def myTestRule(rule_args, callback):
+    where = global_vars['*Where'][1:-1]
+    in_str = global_vars['*StringIn'][1:-1]
+
+    callback.writeLine(where, in_str + ' cheese')
+
+INPUT *Where="stdout", *StringIn="string"
+OUTPUT ruleExecOut
