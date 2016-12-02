e532320932854a013d511f28bf0e8a7e3eaf52f6
Rick Skarbez <skarbez@gmail.com>
2016-12-02 13:22:39 -0500

    [#2] Added rulemsiSubstr.r

---
+++ python_rules/rulemsiSubstr.r
@@ -0,0 +1,16 @@
+def myTestRule(rule_args, callback):
+    in_str = global_vars['*StringIn'][1:-1]
+    offset_str = global_vars['*Offset'][1:-1]
+    length_str = global_vars['*Length'][1:-1]
+
+    offset = int(offset_str)
+    length = int(length_str)
+
+    sub_str = in_str[offset:offset+length]
+
+    callback.writeLine('stdout', 'The input string is: ' + in_str)
+    callback.writeLine('stdout', 'The offset is ' + offset_str + ' and the length is ' + length_str)
+    callback.writeLine('stdout', 'The output string is ' + sub_str)
+
+INPUT *StringIn="/tempZone/home/rods/sub1/foo1/", *Offset="10", *Length="4"
+OUTPUT ruleExecOut
