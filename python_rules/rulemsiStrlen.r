f19ab122d9b144ea86616f12c110fc4596a0d4de
Rick Skarbez <skarbez@gmail.com>
2016-12-02 13:13:07 -0500

    [#2] Added rulemsiStrlen.r

---
+++ python_rules/rulemsiStrlen.r
@@ -0,0 +1,9 @@
+def myTestRule(rule_args, callback):
+    in_str = global_vars['*StringIn'][1:-1]
+
+    length = len(in_str)
+
+    callback.writeLine('stdout', 'The string: ' + in_str + ' has length ' + str(length))
+
+INPUT *StringIn="/tempZone/home/rods/sub1/foo1"
+OUTPUT ruleExecOut
