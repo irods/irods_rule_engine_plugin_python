8c1317cfb7e855ca2fedb50df3827150c1d3ee58
Rick Skarbez <skarbez@gmail.com>
2016-12-02 13:44:21 -0500

    [#2] Added rulewriteLine.r

---
+++ python_rules/rulewriteLine.r
@@ -0,0 +1,8 @@
+def myTestRule(rule_args, callback):
+    where = global_vars['*Where'][1:-1]
+    in_str = global_vars['*StringIn'][1:-1]
+
+    callback.writeLine(where, in_str)
+
+INPUT *Where="stdout", *StringIn="line"
+OUTPUT ruleExecOut
