b73924212ad6970641b351812c1b0ddc2471275a
Zoey Greer <tempoz@renci.org>
2017-06-02 15:55:15 -0400

    installed core file ought to be a template

--- setup_python_rule_engine_as_only_rule_engine.py
+++ setup_python_rule_engine_as_only_rule_engine.py
@@ -1,9 +1,14 @@
 from __future__ import print_function
 
+import os
+import shutil
+
 from irods.configuration import IrodsConfig
 from irods import paths
 
 def main():
+    shutil.copy(os.path.join(paths.config_directory(), 'core.py.template'), os.path.join(paths.config_directory(), 'core.py'))
+
     irods_config = IrodsConfig()
     irods_config.server_config['plugin_configuration']['rule_engines'] = [
             {
