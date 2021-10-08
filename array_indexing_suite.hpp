8a155ea8e247566295b94282be3673aa52137d68
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2021-10-08 11:56:30 -0400

    [#17] Silence warnings about deprecated PyEval_* calls from boost

--- array_indexing_suite.hpp
+++ array_indexing_suite.hpp
@@ -5,9 +5,13 @@
 #include <type_traits>
 
 #include <patchlevel.h>
+#include <boost/version.hpp>
 #pragma GCC diagnostic push
 #if PY_VERSION_HEX < 0x030400A2
 #pragma GCC diagnostic ignored "-Wregister"
+#endif
+#if PY_VERSION_HEX >= 0x03090000 && BOOST_VERSION < 107500
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #endif
 #include <boost/python.hpp>
 #include <boost/python/suite/indexing/indexing_suite.hpp>
