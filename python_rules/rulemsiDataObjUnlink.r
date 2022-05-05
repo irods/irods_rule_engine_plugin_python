8f7356e499e60a4e55475506b0078a02d115f842
Alan King <alanking@renci.org>
2022-05-05 07:15:21 -0400

    [#118] Remove replNum from msiDataObjUnlink example rule

--- python_rules/rulemsiDataObjUnlink.r
+++ python_rules/rulemsiDataObjUnlink.r
@@ -1,9 +1,9 @@
 def main(rule_args, callback, rei):
     source_file = global_vars['*SourceFile'][1:-1]
 
-    callback.msiDataObjUnlink('objPath=' + source_file + '++++replNum=1', 0)
+    callback.msiDataObjUnlink('objPath=' + source_file + '++++forceFlag=', 0)
 
-    callback.writeLine('stdout', 'Replica number 1 of file ' + source_file + ' is removed') 
+    callback.writeLine('stdout', 'Data object ' + source_file + ' is now removed')
 
 INPUT *SourceFile="/tempZone/home/rods/sub1/objunlink2"
 OUTPUT ruleExecOut
