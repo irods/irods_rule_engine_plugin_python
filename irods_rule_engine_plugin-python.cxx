3b7f1cc3753c4fd67cdfa11deb6004bda6f68ad4
d-w-moore <dmoore@renci.org>
2022-06-26 10:25:31 -0400

    [#122] kludge for bionic: load Python3 symbols before importing core
    Co-authored-by: Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>

--- irods_rule_engine_plugin-python.cxx
+++ irods_rule_engine_plugin-python.cxx
@@ -344,8 +344,14 @@ irods::error start(irods::default_re_ctx&, const std::string& _instance_name)
 {
     // TODO Enable config-selectable Python version
 #if PY_VERSION_HEX < 0x03000000
-    dlopen("libpython2.7.so", RTLD_LAZY | RTLD_GLOBAL); // https://mail.python.org/pipermail/new-bugs-announce/2008-November/003322.html
+    void *p = dlopen("libpython2.7.so", RTLD_LAZY | RTLD_GLOBAL); // https://mail.python.org/pipermail/new-bugs-announce/2008-November/003322.html
+#else
+    void *p = dlopen("libpython3.6m.so.1.0", RTLD_LAZY | RTLD_GLOBAL); // Kludge for bionic; proper solution forthcoming.
 #endif
+    rodsLog(LOG_DEBUG, "dlopen returned: [%p]", p);
+    if (!p) {
+       rodsLog(LOG_DEBUG, "dlerror gives : [%s]", dlerror());
+    }
 
     try {
 #if PY_VERSION_HEX < 0x03000000
