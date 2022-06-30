91fcb829fa669f9f4a0607959bc2ae1c9ba3e920
Markus Kitsinger (SwooshyCueb) <kitsinger@renci.org>
2022-06-30 12:45:29 -0400

    [122] Log dlopen failures for python 2

--- irods_rule_engine_plugin-python.cxx
+++ irods_rule_engine_plugin-python.cxx
@@ -344,14 +344,14 @@ irods::error start(irods::default_re_ctx&, const std::string& _instance_name)
 {
     // TODO Enable config-selectable Python version
 #if PY_VERSION_HEX < 0x03000000
-    dlopen("libpython2.7.so", RTLD_LAZY | RTLD_GLOBAL); // https://mail.python.org/pipermail/new-bugs-announce/2008-November/003322.html
+    void *p = dlopen("libpython2.7.so", RTLD_LAZY | RTLD_GLOBAL); // https://mail.python.org/pipermail/new-bugs-announce/2008-November/003322.html
 #else
     void *p = dlopen("libpython3.6m.so.1.0", RTLD_LAZY | RTLD_GLOBAL); // Kludge for bionic; proper solution forthcoming.
+#endif
     rodsLog(LOG_NOTICE, "DWM - dlopen returned: [%p]", p);
     if (!p) {
        rodsLog(LOG_NOTICE, "    - dlerror gives : [%s]", dlerror());
     }
-#endif
 
     try {
 #if PY_VERSION_HEX < 0x03000000
