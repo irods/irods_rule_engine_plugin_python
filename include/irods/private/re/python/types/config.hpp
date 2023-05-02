09c7c1b548c63446b7bd13de1195bde8418d48f1
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2023-05-02 18:41:56 -0400

    [#84] Move BOOST_PYTHON_MAX_ARITY define to new header

---
+++ include/irods/private/re/python/types/config.hpp
@@ -0,0 +1,11 @@
+#ifndef RE_PYTHON_TYPES_CONFIG_HPP
+#define RE_PYTHON_TYPES_CONFIG_HPP
+
+#ifndef BOOST_PYTHON_MAX_ARITY
+#define BOOST_PYTHON_MAX_ARITY 45
+#endif
+
+// include this first to fix macro redef warnings
+#include <pyconfig.h>
+
+#endif // RE_PYTHON_TYPES_CONFIG_HPP
