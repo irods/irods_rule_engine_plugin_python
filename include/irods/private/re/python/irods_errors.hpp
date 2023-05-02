22fa29763b27e78a58fd2267e062a2d1facb09c6
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2023-05-02 19:06:21 -0400

    [#84] Split irods_errors module into separate cpp

---
+++ include/irods/private/re/python/irods_errors.hpp
@@ -0,0 +1,16 @@
+#ifndef RE_PYTHON_IRODS_ERRORS_HPP
+#define RE_PYTHON_IRODS_ERRORS_HPP
+
+// include this first to fix macro redef warnings
+#include <pyconfig.h>
+
+#include <patchlevel.h>
+
+#if PY_VERSION_HEX < 0x03000000
+extern "C" void initirods_errors();
+#else
+#include <Python.h>
+extern "C" PyObject* PyInit_irods_errors();
+#endif
+
+#endif // RE_PYTHON_IRODS_ERRORS_HPP
