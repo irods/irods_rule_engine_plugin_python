4670d267213495a6e0cc5e4263ced05e7103fdc0
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2024-08-12 15:17:38 -0500

    [#217] Remove Python 2 support code

--- include/irods/private/re/python/irods_errors.hpp
+++ include/irods/private/re/python/irods_errors.hpp
@@ -4,13 +4,8 @@
 // include this first to fix macro redef warnings
 #include <pyconfig.h>
 
-#include <patchlevel.h>
+#include <Python.h>
 
-#if PY_VERSION_HEX < 0x03000000
-extern "C" void initirods_errors();
-#else
-#  include <Python.h>
 extern "C" PyObject* PyInit_irods_errors();
-#endif
 
 #endif // RE_PYTHON_IRODS_ERRORS_HPP
